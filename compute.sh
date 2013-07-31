0251ee61e82b6e8b8424551bec05b2cab1d8a21e
VMTrooper <vmtrooper@gmail.com>
2013-07-31 16:55:45 -0700

    controller.sh and compute.sh updated to dynamically generate Cinder IP and input Cinder settings to nova.conf

--- compute.sh
+++ compute.sh
@@ -71,6 +71,12 @@ auto_assign_floating_ip=True
 #metadata_listen = ${CONTROLLER_HOST}
 #metadata_listen_port = 8775
 
+# Cinder #
+volume_driver=nova.volume.driver.ISCSIDriver
+enabled_apis=ec2,osapi_compute,metadata
+volume_api_class=nova.volume.cinder.API
+iscsi_helper=tgtadm
+
 # Images
 image_service=nova.image.glance.GlanceImageService
 glance_api_servers=${GLANCE_HOST}:9292
